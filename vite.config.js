f947dd11d47b69eb0e9150896589a1adf5e74b1e
MW8 <141382500+MW8-ai@users.noreply.github.com>
2026-05-20 00:45:15 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
-  base: "/cloud-matrix/",   // ← change to your repo name
+  base: "/CloudIntelMatrix/",
   build: { outDir: "dist", emptyOutDir: true },
 });
